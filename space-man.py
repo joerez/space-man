a59068fa2b929541606ee0b0ba6eaf6693f1c214
Joerez <rezenbiz@gmail.com>
2018-09-10 10:56:25 -0700

    space man flies

--- space-man.py
+++ space-man.py
@@ -20,14 +20,72 @@ previously_used_letters = []
 space_man = ""
 encrypted_word = ""
 
+def show_space_man():
+    global tries_left
+    global space_man
+    if tries_left == 7:
+        print(space_man)
+    elif tries_left == 6:
+        space_man = """
+        .-'''-.
+       /= ___  l
+      |- /~~~\  |
+      |=( '.' ) |
+      \__\_=_/__/
+      """
+        print(space_man)
+    elif tries_left == 5:
+        space_man = """
+
+        .-'''-.
+       /= ___  l
+      |- /~~~\  |
+      |=( '.' ) |
+      \__\_=_/__/
+       {_______}
+       *       `
+      .      [] .
+      | ooo     |
+       \_______/
+
+        """
+        print(space_man)
+    elif tries_left == 4:
+        space_man == """
+
+        .-'''-.
+       /= ___  l
+      |- /~~~\  |
+      |=( '.' ) |
+      \__\_=_/__/
+       {_______}
+     l` *       `
+    l= .     [] .
+   l  /|ooo     |
+  (   )\_______/
+   l``l
+    `-
+
+        """
+        print(space_man)
+
+
+    else:
+        print(space_man)
+
+
+
 def print_game():
+    show_space_man()
+
     print("\n")
     print("tries left:")
     print(tries_left)
-    print("\n")
-    print(space_man)
+
+
     print("\n")
     print(encrypted_word)
+    print(the_secret_word)
     print("\n")
     print("Previously guessed letters:")
     print(previously_used_letters)
@@ -37,33 +95,55 @@ list_length = len(list_of_words)
 
 the_secret_word = list_of_words[randint(0, list_length) - 1]
 
+
+
 # encrypt word
 # this function looks for all letters a through z in the_secret_word, and replaces them with underscores.
+the_secret_word = the_secret_word.replace("", " ")
 
-encrypted_word = re.sub('[a-z]', '_ ', the_secret_word)
+
+encrypted_word = re.sub('[a-z]', '_', the_secret_word)
+
+
 
 # Space man game
 def space_man():
+    global tries_left
+    global encrypted_word
     while (tries_left > 0):
         print_game()
-        letter_guess = input("\nGuess a letter: ")
-        if letter_guess not in previously_used_letters:
-            previously_used_letters.append(word_guess)
+        if '_' in encrypted_word:
+            letter_guess = input("\nGuess a letter: ")
+            if letter_guess not in previously_used_letters:
+                previously_used_letters.append(letter_guess)
 
-            if letter_guess in the_secret_word:
-                print("letter found")
+                if letter_guess in the_secret_word:
+                    print("letter found")
+                    # check which index(es) in secret word, it belongs to
+                    correct_letter_indices = []
+                    for i, character in enumerate(the_secret_word):
+                        if the_secret_word[i] == letter_guess:
+                            correct_letter_indices.append(i)
+
+                    # change the index(es) in the encrypted word to have the letter(s)
+                    encrypted_word_list = list(encrypted_word)
+                    for index in correct_letter_indices:
+                        encrypted_word_list[index] = letter_guess
+                    encrypted_word = "".join(encrypted_word_list)
+
+
+                else:
+                    print("letter not found")
+                    tries_left = tries_left - 1
+
             else:
-                print("letter not found")
-                tries_left - 1
-
-
-
-
-
-
+                print("you already guessed that letter!")
         else:
-            print("you already guessed that letter!")
-
+            print("you win")
+            break
+    if (tries_left == 0):
+        print('you lose')
+        print(the_secret_word)
 
 
 space_man()
